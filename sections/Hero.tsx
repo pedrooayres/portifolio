81f65d4ecb2c4ff0031515b0ed42cddf3fb733f8
pedrooayres <pedrohayres@yahoo.com.br>
2026-03-05 00:11:55 +0000

    Foto de perfil

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,19 +1,18 @@
+import Image from "next/image"
 export default function Hero() {
   return (
     <section className="hero">
       <div className="container hero-content">
-        <h1>Desenvolvedor Frontend focado em construir aplicações web modernas</h1>
+        <Image src="/public/fotoPedro.png"alt="Pedro Ayres" width={150}height={150}className="profile-photo"/>
+        <h1>Pedro Ayres</h1>
         <p>
-          Trabalho com React, Next.js e TypeScript criando interfaces
-          organizadas, performáticas e com boa experiência de uso.
+          Desenvolvedor interessado em tecnologia e construção de aplicações web modernas.
+          Trabalho com React, Next.js e TypeScript, além de ter experiência com Java e estruturas de dados.
+          Busco constantemente aprender novas ferramentas e aprimorar minhas habilidades de desenvolvimento.
         </p>
         <div className="hero-actions">
-          <a href="#projetos" className="button-neon">
-            Ver projetos
-          </a>
-          <a href="#contato" className="button-outline">
-            Entrar em contato
-          </a>
+          <a href="#projetos" className="button-neon">Ver projetos</a>
+          <a href="#contato" className="button-outline">Entrar em contato</a>
         </div>
       </div>
     </section>
